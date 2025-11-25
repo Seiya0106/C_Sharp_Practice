e8d1fb8fdd3527eed1bae8d4e120c6069622184e
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2025-11-25 17:23:55 +0900

    6_8

---
+++ List/6_8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sample6_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            dic["cat"] = "猫";
+            dic["dog"] = "犬";
+            dic["bird"] = "鳥";
+            dic["tiger"] = "トラ";
+            Console.Write("英語で動物の名前を入力してください：");
+            string input = Console.ReadLine();
+            Console.WriteLine("「{0}」です", dic[input]);
+        }
+    }
+}
