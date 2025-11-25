5bbb4eb131118526f9c37352d5441cea27b80317
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2025-11-25 10:45:52 +0900

    7_1

---
+++ Access/7_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Data
+{
+    //  メンバ変数number
+    private int number = 0;
+    //  メンバ変数comment
+    private string comment = "";
+    // 数字の設定
+    public int Number
+    {
+        set { number = value; }
+        get { return number; }
+    }
+    public string Comment
+    {
+        set { comment = value; }
+        get { return comment; }
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Data d = new Data();
+        d.Number = 100;
+        d.Comment = "Programming C#";
+        Console.WriteLine("number = " + d.Number);
+        Console.WriteLine("comment = " + d.Comment);
+    }
+}
