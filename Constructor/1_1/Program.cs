86fd4d4c09e91a96fc639f47e70d5b387044d16b
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2025-11-25 20:18:13 +0900

    practice

---
+++ Constructor/1_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace sample1_1
+{
+    class Sample
+    {
+        public Sample()
+        {
+            Console.WriteLine("コンストラクタ");
+        }
+        public void foo()
+        {
+            Console.WriteLine("foo");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample s = new Sample();
+            s.foo();
+        }
+    }
+}
---
+++ Constructor/1_2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace sample1_2
+{
+    class Calc
+    {
+        public Calc()
+        {
+            num1 = 0;
+            num2 = 0;
+        }
+        public Calc(int num1, int num2)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+        }
+        //  一つ目の数
+        private int num1;
+        //  二つ目の数
+        private int num2;
+        //  一つ目の数のプロパティ
+        public int Num1
+        {
+            set { num1 = value; }
+            get { return num1; }
+        }
+        //  二つ目の数のプロパティ
+        public int Num2
+        {
+            set { num2 = value; }
+            get { return num2; }
+        }
+        public void ShowAdd()
+        {
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, num1 + num2);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc c1 = new Calc(),c2 = new Calc(3,1);
+            c1.Num1 = 1;
+            c1.Num2 = 2;
+            //  加算の結果を表示
+            c1.ShowAdd();
+            c2.ShowAdd();
+        }
+    }
+}
