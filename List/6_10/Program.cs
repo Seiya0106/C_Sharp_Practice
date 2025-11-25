28f1672596e2628904f74fdce89f3e322d850137
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2025-11-25 17:30:01 +0900

    6_10

---
+++ List/6_10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sample6_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet <int> s = new HashSet<int>();
+            Random rnd = new Random();
+            Console.Write("乱数：");
+            for (int i = 0; i < 10; i++)
+            {
+                int num = rnd.Next(1, 11);
+                Console.Write(num + " ");
+                s.Add(num);
+            }
+            Console.Write("\n出現した数：");
+            foreach (int n in s)
+            {
+                Console.Write(n + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
