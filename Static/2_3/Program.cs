a5ca6380f902f0aed722738b419920dbc25e78e3
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2025-11-25 23:29:25 +0900

    2_3

---
+++ Static/2_3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Square
+{
+    public static void DrawSquare(int line, int col)
+    {
+        for (int i = 0; i < col; i++)
+        {
+            for (int j = 0; j < line; j++)
+            {
+                Console.Write("■");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        int line = rnd.Next(1, 11);
+        int col = rnd.Next(1, 11);
+        Console.WriteLine("たて：{0}", line);
+        Console.WriteLine("よこ：{0}", col);
+        Console.WriteLine();
+        Square.DrawSquare(line, col);
+    }
+}
