ac68a5327014fa7fe24c8c1390e61859025b50ba
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2025-11-25 09:32:24 +0900

    interface

---
+++ Interface/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Sample
+{
+    // インターフェース
+    public interface IMessage
+    {
+        void Hello();
+    }
+
+    // インターフェースを実装するクラス
+    class View : IMessage
+    {
+        public void Morning()
+        {
+            Console.WriteLine("おはよう");
+        }
+        public void Hello()
+        {
+            Console.WriteLine("こんにちは");
+        }
+    }
+
+    // メインクラス
+    class Sample
+    {
+        static void Main(string[] args)
+        {
+            View vw = new View();
+
+            vw.Morning();
+            vw.Hello();
+        }
+    }
+}
