76e3db67beaa27849a60d3ecf1dcb8228ab1ac59
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2025-11-25 09:58:59 +0900

    5_8

---
+++ Hairetsu/5_8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int [] data = new int[5];
+        Random rnd = new Random();
+        for (int i = 0; i < data.Length; i++)
+        {
+            data[i] = rnd.Next(-10, 11);
+            Console.Write(data[i] + " ");
+        }
+        Console.WriteLine();
+        int cntOver = 0;
+        int cntUnder = 0;
+        for (int i = 0; i < data.Length; i++)
+        {
+            if (data[i] > 0)
+            {
+                cntOver++;
+            }
+            else if (data[i] < 0)
+            {
+                cntUnder++;
+            }
+        }
+        Console.WriteLine("0より大きい数：" + cntOver + "個");
+        Console.WriteLine("0より小さい数：" + cntUnder + "個");
+        int cntZero = data.Length - cntOver - cntUnder;
+        Console.WriteLine("0の数：" + cntZero + "個");
+    }
+}
