f2b0de4c6be0c364a742fe3dcde208721899d2e8
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2025-11-25 17:06:14 +0900

    hashset

---
+++ List/Sample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> a = new List<string>();
+            // データを追加
+            a.Add("Taro");
+            a.Add("Hanako");
+            a.Add("Jiro");
+            a.Add("Kaoru");
+            // データを削除
+            a.Remove("Taro");
+            a.RemoveAt(1);
+            foreach (string s in a)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
