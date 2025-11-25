8c7492cbe5f34aed656e9b7dc0872eca30976973
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2025-11-26 08:47:52 +0900

    3_1

---
+++ Keisyo/3_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Airplane
+    {
+        public void Fly(){
+            Console.WriteLine("飛行する");
+        }
+    }
+
+class Fighter : Airplane
+{
+    public void Fight()
+    {
+        Console.WriteLine("戦闘する");
+    }
+}
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Fighter f = new Fighter();      //  戦闘機クラス
+            Airplane a = new Airplane();    //  飛行機クラス
+            //  飛行機が飛行する
+            a.Fly();
+            //  戦闘機が飛行する
+            f.Fly();
+            //  戦闘機が戦闘する
+            f.Fight();
+        }
+    }
