5f4a64daf5e0ec298720f1ade0f2e9ffea0bfb4e
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2025-11-26 09:15:37 +0900

    sample

---
+++ Keisyo/Sample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace sample
+{
+    class Parent
+    {
+        public virtual void Foo()
+        {
+            Console.WriteLine("親クラスのFoo()メソッド");
+        }
+    }
+    class Child : Parent
+    {
+        public override void Foo()
+        {
+            Console.WriteLine("子クラスのFoo()メソッド");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Parentクラスのインスタンス生成
+            Parent p = new Parent();
+            //  Childクラスのインスタンス生成
+            Parent c = new Child();
+            //  それぞれのクラスのfoo、barメソッドを実行
+            p.Foo();
+            c.Foo();
+        }
+    }
+}
