8dd1fd6091d82080d1de9c314c2ddd14639dd3e1
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2025-11-26 12:45:03 +0900

    4_1

---
+++ Abstract/4_1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Problem4_1
+{
+    abstract class Animal
+    {
+        protected string name = "";
+        public abstract void Bark();
+        public abstract void Move();
+        public void ShowName()
+        {
+            Console.WriteLine("名前：{0}", name);
+        }
+    }
+    
+    class Dog : Animal
+    {
+        public Dog()
+        {
+            name = "犬";
+        }
+        public override void Bark()
+        {
+            Console.WriteLine("ワンワン");
+        }
+        public override void Move()
+        {
+            Console.WriteLine("歩く");
+        }
+    }
+
+    class Monkey : Animal
+    {
+        public Monkey()
+        {
+            name = "猿";
+        }
+        public override void Bark()
+        {
+            Console.WriteLine("キーキー");
+        }
+        public override void Move()
+        {
+            Console.WriteLine("木に登る");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Animalクラスのオブジェクトの配列を生成
+            Animal[] animals = new Animal[2];
+            animals[0] = new Dog();     //  犬クラスのインスタンス生成
+            animals[1] = new Monkey();  // 猿クラスのインスタンスの生成
+            for (int i = 0; i < animals.Length; i++)
+            {
+                animals[i].ShowName();
+                animals[i].Move();
+                animals[i].Bark();
+                Console.WriteLine("------------");
+            }
+        }
+    }
+}
