67a4cdb9da31161ecae79bc7ba70badd8cffeed1
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2025-11-29 17:54:29 +0900

    試しにAtCoderのA問題を解いてみた

---
+++ Kyopuro/A/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[]input = Console.ReadLine().Split(" ");
+            int r = int.Parse(input[0]);
+            int g = int.Parse(input[1]);
+            int b = int.Parse(input[2]);
+            string c = Console.ReadLine();
+            if (c == "Red")
+            {
+                if (g > b)
+                {
+                    Console.WriteLine(b);
+                }
+                else
+                {
+                    Console.WriteLine(g);
+                }
+            }
+            else if (c == "Green")
+            {
+                if (r > b)
+                {
+                    Console.WriteLine(b);
+                }
+                else
+                {
+                    Console.WriteLine(r);
+                }
+            }
+            else
+            {
+                if (r > g)
+                {
+                    Console.WriteLine(g);
+                }
+                else
+                {
+                    Console.WriteLine(r);
+                }
+            }
+        }
+    }
+}
