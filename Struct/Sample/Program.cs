2a30362d898aae3025d5535a3d57ca9dea7e24d9
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2025-11-29 16:55:16 +0900

    Add; StructSample

---
+++ Struct/Sample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sample
+{
+    public struct Data
+    {
+        public int id;
+        public List<string> names;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Data db = new Data();
+            db.id = 1;
+            db.names = new List<string>();
+            db.names.Add("Alice");
+            db.names.Add("Bob");
+            Console.WriteLine("ID: {0}", db.id);
+            Console.WriteLine("Names:");
+            foreach (string name in db.names)
+            {
+                Console.WriteLine("- " + name);
+            }
+        }
+    }
+}
